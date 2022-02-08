958967834a2f6aeae9a6480d081b84304e7c54df
drensexy69 <99254151+drensexy69@users.noreply.github.com>
2022-02-08 02:19:33 -1100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.Prefix = `&`;//your prefix for bot
-exports.Token = `OTM5NzEzODAyMjc3MTEzODg3.Yf82rQ.YWEWjrCRIwX50EUF_HgpusgpFJc`;//your token 
+exports.Token = `OTM5NzEzODAyMjc3MTEzODg3.Yf82rQ.XmaLwzTkxL1Yvir7tKLZmeFWv9M`;//your token 
 exports.Color = `RED`;//color   of embed
